78e3d70bc9338ce7a9b4b0b0339af9b36e27fb03
Buyaki01 <rittahbuyaki@gmail.com>
2023-12-28 09:30:48 +0300

    Add project images, add projects to the Project component

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,41 @@
 import './projects.css'
+import pearlsCollections from '../../assets/pearlsCollections.PNG'
+import airbnb from '../../assets/airbnb.PNG'
+import swimmingBlog from '../../assets/swimmingBlog.PNG'
 
 export const Projects = () => {
   return (
     <section id='projects'>
-      Projects
+      <h5>My Recent Work</h5>
+      <h2>Projects</h2>
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={pearlsCollections} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/Buyaki01" className='btn' target='_blank'>Github</a>
+          <a href="https://github.com/Buyaki01/nextjs13-online-store-shop" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={airbnb} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/Buyaki01" className='btn' target='_blank'>Github</a>
+          <a href="https://github.com/Buyaki01/react-redux-airbnb-client" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={swimmingBlog} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className='portfolio__item-cta'>
+            <a href="https://github.com/Buyaki01" className='btn' target='_blank'>Github</a>
+            <a href="https://github.com/Buyaki01/swimming-blog-nextjs" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+      </div>
     </section>
   )
 }
