6eb0c687547493fd509f28b950cb674e3b11ba58
Buyaki01 <rittahbuyaki@gmail.com>
2023-12-29 11:28:00 +0300

    Uninstall unused packages, install react-slick and slick-carousel, Add brand logo images of my skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,79 +1,91 @@
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"
 import './skills.css'
-import Carousel from "react-multi-carousel"
-import "react-multi-carousel/lib/styles.css"
-import { SiNextdotjs, SiExpress } from "react-icons/si"
-import { FaReact } from "react-icons/fa"
-import { IoLogoJavascript, IoLogoNodejs } from "react-icons/io5"
-import { DiMongodb } from "react-icons/di"
-import { BiLogoTailwindCss } from "react-icons/bi"
-import { BiLogoPostgresql } from "react-icons/bi"
+import tailwindCssLogo from '../../assets/Tailwind-CSS.png'
+import mongodbLogo from '../../assets/mongodbLogo.png'
+import nextjslogo from '../../assets/nextjslogo.png'
+import reactLogo from '../../assets/reactLogo.png'
+import nodejsLogo from '../../assets/nodejs-logo.png'
+import expressLogo from '../../assets/expressLogo.png'
+import postgresql from '../../assets/postgresql.png'
+import javascriptLogo from '../../assets/javascriptlogo.png'
+
+const data = [
+  {
+    image: reactLogo,
+    name: 'React'
+  }, 
+  {
+    image: nextjslogo,
+    name: 'NextJs'
+  }, 
+  {
+    image: javascriptLogo,
+    name: 'JavaScript'
+  },
+  {
+    image: nodejsLogo,
+    name: 'NodeJs'
+  }, 
+  {
+    image: expressLogo,
+    name: 'ExpressJs'
+  }, 
+  {
+    image: tailwindCssLogo,
+    name: 'TailwindCss'
+  }, 
+  {
+    image: mongodbLogo,
+    name: 'mongoDB'
+  }, 
+  {
+    image: postgresql,
+    name: 'postgresql'
+  }, 
+]
 
 export const Skills = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024, // Medium devices (tablets)
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768, // Small devices (phones)
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   }
 
   return (
-    <section className="skill" id="skills">
-      <div>
-        <div>
-          <div>
-            <div className="skill-bx">
-              <h2>Skills</h2>
-              <Carousel responsive={responsive} infinite={true} className="skill-slider">
-                <div className="item">
-                  <SiNextdotjs class="w-50 h-50" />
-                  <h5 className="mt-2">NextJS</h5>
-                </div>
-                <div className="item">
-                  <FaReact class="w-50 h-50"/>
-                  <h5>React</h5>
-                </div>
-                <div className="item">
-                  <IoLogoJavascript class="w-50 h-50" />
-                  <h5>JavaScript</h5>
-                </div>
-                <div className="item">
-                  <IoLogoNodejs class="w-50 h-50" />
-                  <h5>NodeJs</h5>
-                </div>
-                <div className="item">
-                  <SiExpress class="w-50 h-50" />
-                  <h5>ExpressJS</h5>
-                </div>
-                <div className="item">
-                  <DiMongodb class="w-50 h-50"/>
-                  <h5>MongoDB</h5>
-                </div>
-                <div className="item">
-                  <BiLogoTailwindCss class="w-50 h-50" />
-                  <h5>TailwindCss</h5>
-                </div>
-                <div className="item">
-                  <BiLogoPostgresql class="w-50 h-50" />
-                  <h5>postGRES</h5>
-                </div>
-              </Carousel>
-            </div>
-          </div>
-        </div>
-      </div>
+    <section id="skills">
+      <h2>Skills</h2>
+      <Slider {...settings} className="container skills__container">
+        {data.map(({image, name}, index) => {
+          return (
+            <article key={index} className="skill flex gap-2">
+              <div className="logo__skill">
+                <img src={image} alt={name} />
+              </div>
+              <h5 className='skill__name'>{name}</h5>
+            </article>
+          )
+        })}
+      </Slider>
     </section>
   )
 }
