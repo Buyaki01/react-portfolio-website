71eb4c96d3601c158f442b0c76612fa1a2683fd2
Buyaki01 <rittahbuyaki@gmail.com>
2024-01-03 11:40:50 +0300

    Add to the footer component and add styling to it

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,22 @@
 import './footer.css'
+import { GrGithub } from "react-icons/gr"
+import { FaLinkedin } from "react-icons/fa"
 
 export const Footer = () => {
   return (
-    <section id='footer'>
-      Footer
-    </section>
+    <footer>
+      <a href="#">RITTA SWETA</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://github.com/Buyaki01"><GrGithub /></a>
+        <a href="https://www.linkedin.com/in/ritta-sweta"><FaLinkedin /></a>
+      </div>
+    </footer>
   )
 }
