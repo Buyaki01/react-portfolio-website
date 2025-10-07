3fe56e2d38d427ae99f832b2ab4d11e0245f156f
ritta.sweta <rittahbuyaki@gmail.com>
2025-10-07 07:24:12 +0300

    Update my resume

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,29 @@
-import { HeaderButtons } from './HeaderButtons'
-import './header.css'
-import Me from '../../assets/me.png'
-import { HeaderSocials } from './HeaderSocials'
+import Me from "../../assets/me.png";
+import { HeaderButtons } from "./HeaderButtons";
+import { HeaderSocials } from "./HeaderSocials";
+import "./header.css";
 
 export const Header = () => {
-  return (
-    <header>
-      <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Ritta Sweta</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
-        <HeaderButtons />
-        <HeaderSocials />
-        <div className="me">
-          <img src={Me} alt="Professional Photo" />
-        </div>
+    return (
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Ritta Sweta</h1>
+                <h5 className="text-light">
+                    A Full Stack Software Developer with over 5 years of
+                    experience turning ideas into impactful digital solutions
+                    across Healthcare, Fintech, and eCommerce.
+                </h5>
+                <HeaderButtons />
+                <HeaderSocials />
+                <div className="me">
+                    <img src={Me} alt="Professional Photo" />
+                </div>
 
-        <a href="#contact" className="scroll__down">Scroll Down</a>
-      </div>
-    </header>
-  )
-}
+                <a href="#contact" className="scroll__down">
+                    Scroll Down
+                </a>
+            </div>
+        </header>
+    );
+};
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,74 +1,148 @@
-import "./projects.css";
-import pearlsCollections from "../../assets/pearlsCollections.PNG";
 import airbnb from "../../assets/airbnb.PNG";
+import pearlsCollections from "../../assets/pearlsCollections.PNG";
 import pearlsThamaniCare from "../../assets/pearlsThamaniCare.png";
+import carRental from "../../assets/thamani-cars.png";
+import "./projects.css";
 
 const data = [
-  {
-    id: 1,
-    image: pearlsCollections,
-    title: "Pearls Collections Shop",
-    github: "https://github.com/Buyaki01/nextjs-online-store-shop",
-    demo: "https://pearls-thamani-collections.vercel.app",
-    isPrivate: true,
-  },
-  {
-    id: 2,
-    image: airbnb,
-    title: "Airbnb",
-    github: "https://github.com/Buyaki01/react-redux-airbnb-client",
-    demo: "https://react-redux-airbnb-client.onrender.com",
-    isPrivate: true,
-  },
-  {
-    id: 3,
-    image: pearlsThamaniCare,
-    title: "Pearls Thamani Care",
-    github: "https://github.com/Buyaki01/pearls-thamani-care",
-    demo: "https://pearls-thamani-care.vercel.app/home",
-    isPrivate: true,
-  },
+    {
+        id: 1,
+        image: airbnb,
+        title: "Airbnb",
+        github: "https://github.com/Buyaki01/react-redux-airbnb-client",
+        demo: "https://react-redux-airbnb-client.onrender.com",
+        isPrivate: true,
+        languages: [
+            "React",
+            "Redux Toolkit",
+            "Node.js",
+            "Express",
+            "MongoDB",
+            "Stripe Payment",
+            "TailwindCSS",
+        ],
+    },
+    {
+        id: 2,
+        image: pearlsCollections,
+        title: "Pearls Collections Shop",
+        github: "https://github.com/Buyaki01/nextjs-online-store-shop",
+        demo: "https://pearls-thamani-collections.vercel.app",
+        isPrivate: true,
+        languages: [
+            "Next.js",
+            "React.js",
+            "JavaScript",
+            "MongoDB",
+            "Stripe",
+            "NextAuth",
+            "React",
+        ],
+    },
+    {
+        id: 3,
+        image: carRental,
+        title: "Car Rental",
+        github: "https://github.com/Buyaki01/carrental-mern",
+        demo: "https://thamani-cars.vercel.app",
+        isPrivate: true,
+        languages: [
+            "React",
+            "Redux Toolkit",
+            "React Query",
+            "Node.js",
+            "Express",
+            "MongoDB",
+            "TailwindCSS",
+        ],
+    },
+    {
+        id: 4,
+        image: pearlsThamaniCare,
+        title: "Pearls Thamani Care",
+        github: "https://github.com/Buyaki01/pearls-thamani-care",
+        demo: "https://pearls-thamani-care.vercel.app/home",
+        isPrivate: true,
+        languages: [
+            "Next.js",
+            "React.js",
+            "JavaScript",
+            "MongoDB",
+            "NextAuth",
+            "Tailwind CSS",
+            "Material UI",
+        ],
+    },
 ];
 
 export const Projects = () => {
-  return (
-    <section id="projects">
-      <h5>My Recent Work</h5>
-      <h2>Projects</h2>
-      <div className="container portfolio__container">
-        {data.map(({ id, image, title, github, demo, isPrivate }) => {
-          return (
-            <article key={id} className="portfolio__item">
-              <div className="portfolio__item-image">
-                <img src={image} alt={title} />
-              </div>
-              <h3 className="whitespace-nowrap">{title}</h3>
-              <div className="portfolio__item-cta">
-                {!isPrivate ? (
-                  <a
-                    href={github}
-                    className="btn"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    GitHub
-                  </a>
-                ) : (
-                  <span className="btn-disabled">Private Repository</span>
+    return (
+        <section id="projects">
+            <h5>My Recent Work</h5>
+            <h2>Projects</h2>
+            <div className="container portfolio__container">
+                {data.map(
+                    ({
+                        id,
+                        image,
+                        title,
+                        github,
+                        demo,
+                        isPrivate,
+                        languages,
+                    }) => (
+                        <article
+                            key={id}
+                            className="portfolio__item flex flex-col"
+                        >
+                            <div className="portfolio__item-image">
+                                <img src={image} alt={title} />
+                            </div>
+
+                            <div>
+                                <h3 className="whitespace-nowrap">{title}</h3>
+
+                                {/* Technologies used */}
+                                <div className="portfolio__languages">
+                                    {languages.map((lang, index) => (
+                                        <span
+                                            key={index}
+                                            className="language-badge"
+                                        >
+                                            {lang}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+
+                            <div className="portfolio__item-cta mt-3">
+                                {!isPrivate ? (
+                                    <a
+                                        href={github}
+                                        className="btn"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        GitHub
+                                    </a>
+                                ) : (
+                                    <span className="btn-disabled">
+                                        Private Repository
+                                    </span>
+                                )}
+                                <a
+                                    href={demo}
+                                    className="btn btn-primary"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Live Demo
+                                </a>
+                            </div>
+                        </article>
+                    )
                 )}
-                <a
-                  href={demo}
-                  className="btn btn-primary"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Live Demo
-                </a>
-              </div>
-            </article>
-          );
-        })}
-      </div>
-    </section>
-  );
+            </div>
+        </section>
+    );
 };
